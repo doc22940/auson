8598a02fe37500e04138af5ab59617d81abb99fc
nelsontky <nelson@ccb.wtf>
2020-05-25 22:45:24 +0800

    Updates

---
+++ component---src-pages-404-js-459aefad2b3b2073f8c1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,n,e){"use strict";e.r(n);e("q1tI");var u=e("Bl7J"),s=e("qKvR");n.default=function(){return Object(s.b)(u.a,null,Object(s.b)("h1",null,"NOT FOUND"),Object(s.b)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-459aefad2b3b2073f8c1.js.map
